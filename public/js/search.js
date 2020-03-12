71e4a7a4a60a86d43ca05522fc5354b5d86a94ed
Brittany Morris <brittanymorrisdesign@gmail.com>
2020-03-11 20:33:32 -0400

    Added search bar functionality and recipe cards

--- public/js/search.js
+++ public/js/search.js
@@ -1,20 +1,156 @@
 $(document).ready(function() {
+  $(".recipeCards").empty();
+  //Random Recipe
+  let queryURL =
+    "https://api.spoonacular.com/recipes/random?number=1&apiKey=0bf3d04ff9744651aa6c76a5548b186a";
+
+  $.ajax({
+    url: queryURL,
+    method: "GET"
+  }).then(function(response) {
+    //console.log(response);
+
+    let results = response.recipes;
+
+    for (i = 0; i < results.length; i++) {
+      let queryURL =
+        "https://api.spoonacular.com/recipes/" +
+        results[i].id +
+        "/summary?apiKey=0bf3d04ff9744651aa6c76a5548b186a";
+
+      $.ajax({
+        url: queryURL,
+        method: "GET"
+      }).then(function(response) {
+        console.log(response);
+        let title = response.title;
+        let summary = response.summary;
+        let imageURL =
+          "https://spoonacular.com/recipeImages/" +
+          response.id +
+          "-312x231.jpg";
+
+        // Cards
+        let cardDiv = $("<div>");
+        cardDiv.addClass("card mb-3").addClass("recipe-card");
+        cardDiv.attr("data-recipeId", response.id);
+
+        let row = $("<div>");
+        row.addClass("row").addClass("no-gutters");
+        let imgCol = $("<div>");
+        imgCol.addClass("col-md-4");
+
+        let img = $("<img>");
+        img.addClass("card-img");
+        img.attr("src", imageURL);
+
+        let contentCol = $("<div>");
+        contentCol.addClass("col-md-8");
+
+        let bodyDiv = $("<div>");
+        bodyDiv.addClass("card-body");
+
+        let a = $("<a>");
+        let h5 = $("<h5>");
+        h5.addClass("card-title");
+        h5.text(title);
+        a.append(h5);
+
+        let p = $("<p>");
+        p.addClass("card-text");
+        p.html(summary);
+
+        bodyDiv.append(a).append(p);
+        contentCol.append(bodyDiv);
+        imgCol.append(img);
+        row.append(imgCol).append(contentCol);
+        cardDiv.append(row);
+        $(".recipeCards").append(cardDiv);
+      });
+    }
+  });
+
+  // Searched Recipes
   $("#searchButton").on("click", function() {
-    const query = $("#recipeSearch")
-      .val()
-      .trim();
-    const search = { searchTerm: query };
+    let recipeSearch = $("#searchTerm").val();
+    console.log(recipeSearch);
+
+    $(".recipeCards").empty();
+
+    let queryURL =
+      "https://api.spoonacular.com/recipes/search?query=" +
+      recipeSearch +
+      "&number=4&apiKey=0bf3d04ff9744651aa6c76a5548b186a";
+
     $.ajax({
-      url: "/api/search",
-      method: "POST",
-      data: search
-    }).then(result => console.log(result));
-    // $.ajax({
-    //   // call the backend 'search' route, which will query spoonacular api
-    //   url: "/api/search",
-    //   method: "GET"
-    // }).then(result => {
-    //   console.log(result);
-    // });
+      url: queryURL,
+      method: "GET"
+    }).then(function(response) {
+      console.log(response);
+
+      let results = response.results;
+
+      for (i = 0; i < results.length; i++) {
+        let queryURL =
+          "https://api.spoonacular.com/recipes/" +
+          results[i].id +
+          "/summary?apiKey=0bf3d04ff9744651aa6c76a5548b186a";
+
+        $.ajax({
+          url: queryURL,
+          method: "GET"
+        }).then(function(response) {
+          console.log(response);
+          let title = response.title;
+          let summary = response.summary;
+          let imageURL =
+            "https://spoonacular.com/recipeImages/" +
+            response.id +
+            "-312x231.jpg";
+
+          // Print favorites
+          let cardDiv = $("<div>");
+          cardDiv.addClass("card mb-3").addClass("recipe-card");
+          cardDiv.attr("data-recipeId", response.id);
+
+          let row = $("<div>");
+          row.addClass("row").addClass("no-gutters");
+
+          let imgCol = $("<div>");
+          imgCol.addClass("col-md-4");
+
+          let img = $("<img>");
+          img.addClass("card-img");
+          img.attr("src", imageURL);
+
+          let contentCol = $("<div>");
+          contentCol.addClass("col-md-8");
+
+          let bodyDiv = $("<div>");
+          bodyDiv.addClass("card-body");
+
+          let a = $("<a>");
+          let h5 = $("<h5>");
+          h5.addClass("card-title");
+          h5.text(title);
+          a.append(h5);
+
+          let p = $("<p>");
+          p.addClass("card-text");
+          p.html(summary);
+
+          bodyDiv.append(a).append(p);
+          contentCol.append(bodyDiv);
+          imgCol.append(img);
+          row.append(imgCol).append(contentCol);
+          cardDiv.append(row);
+          $(".recipeCards").append(cardDiv);
+        });
+      }
+    });
+  });
+  $(document).on("click", ".recipe-card", function() {
+    let recipeID = $(this).attr("data-recipeId");
+    localStorage.setItem("recipeId", recipeID);
   });
 });
