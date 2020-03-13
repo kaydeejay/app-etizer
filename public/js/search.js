027cb60b31dcb02b6f9ea7e0c10fa4b802a79e9f
brittanieb <brittanie.boyko@gmail.com>
2020-03-12 23:48:04 -0400

    removed comments and deleted results.js

--- public/js/search.js
+++ public/js/search.js
@@ -76,36 +76,18 @@ $(document).ready(function() {
     const query = $("#recipeSearch")
       .val()
       .trim();
-    // const search = { searchTerm: query };
     window.location.replace("/api/search/" + query);
-    // $.ajax({
-    //   url: `/api/chicken/${query}`,
-    //   method: "POST",
-    //   data: search
-    // }).then(result => console.log(result));
   });
 
   $(document).ready(function() {
     $(function() {
       $(".suggestedSearch").on("click", function() {
-        let searchTerm = $(this)
-          .text()
-          .trim();
-        // console.log("query", query);
-        // }).then(result => {
-        // console.log("result", result);
-        // location.href = "/results.html";
+        let searchTerm = $(this).text();
         $.ajax({
           url: `/api/search/${searchTerm}`,
           method: "POST"
         }).then(function(response) {
           console.log(response);
-          // console.log("LOOK HERE", response);
-          // for (let i = 0; i < response.results.length; i++) {
-          // xtitle = response.results[i].title;
-          // const liTag = $("<li>" + xtitle + "</li>");
-          // const recipeHolder = $("#recipeId");
-          // recipeHolder.append(liTag);
         });
       });
     });
